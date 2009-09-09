dd706cb408f3d8ae437e6e1035f7b206929f35d2
Nick Sieger <nick@nicksieger.com>
2009-09-09 08:01:42 +0200

    Clean up java class

--- app/HelloWorldResource.java
+++ app/HelloWorldResource.java
@@ -4,18 +4,15 @@ import javax.ws.rs.*;
 
 @Path("/helloworld")
 public class HelloWorldResource {
-
-    // The Java method will process HTTP GET requests
     @GET
-    // The Java method will produce content identified by the MIME Media
-    // type "text/plain"
     @Produces("text/plain")
     public String getMessage() {
-        // Return some cliched textual content
         return "Hello World";
     }
 
-    @GET @Produces("text/plain") @Path("{id}")
+    @GET
+    @Produces("text/plain")
+    @Path("{id}")
     public String getPersonalMessage(@PathParam("id") String message) {
         return "Hello " + message;
     }
